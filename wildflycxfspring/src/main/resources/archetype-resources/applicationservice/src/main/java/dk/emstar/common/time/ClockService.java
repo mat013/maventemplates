cde173885d6189e9e712cd0cf7715b5a8819f322
mat013 <mat013@8c0d3757-8840-4923-84ec-c338a6bcf567>
2014-10-17 22:50:27 +0000

    Adding javaconfig
    git-svn-id: svn://dkcphbackup01/work/trunk@511 8c0d3757-8840-4923-84ec-c338a6bcf567

---
+++ wildflycxfspring/src/main/resources/archetype-resources/applicationservice/src/main/java/dk/emstar/common/time/ClockService.java
@@ -0,0 +1,15 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+package dk.emstar.common.time;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZonedDateTime;
+
+public interface ClockService {
+    LocalDateTime now();
+    LocalDate dateNow();
+    ZonedDateTime nowLocal();
+    ZonedDateTime nowUTC();
+}
