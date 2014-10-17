1c7fd37f8305e646fbf6ee20d3265a69984fb64d
mat013 <mat013@8c0d3757-8840-4923-84ec-c338a6bcf567>
2014-10-17 19:27:38 +0000

    Adding better template for quickstart Wildfly, Spring, CXF
    git-svn-id: svn://dkcphbackup01/work/trunk@504 8c0d3757-8840-4923-84ec-c338a6bcf567

---
+++ wildflycxfspring/applicationservice/src/main/java/springcfx/GreetingServiceImpl.java
@@ -0,0 +1,33 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+package ${package}.${parentArtifactId};
+ 
+import javax.inject.Inject;
+import javax.jws.WebService;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+
+import ${package}.${parentArtifactId}.dependency.CalendarService;
+import dk.emstar.webservice.example.Greet;
+import dk.emstar.webservice.example.GreetResponse;
+import dk.emstar.webservice.example.GreetService;
+ 
+@Service
+@WebService
+public class GreetingServiceImpl implements GreetService {
+ 
+    @Inject
+    @Qualifier("calendarService")
+    private CalendarService calendarService;
+
+
+    @Override
+    public GreetResponse greet(Greet request) {
+        GreetResponse response = new GreetResponse();
+        response.setReturn("Hello " + request.getNavn() + " time is " + calendarService.now());
+        
+        return response;
+    }
+}
---
+++ wildflycxfspring/interfaces/src/main/java/dk/emstar/jaxb/adapters/UTCLocalDateTimeAdapter.java
@@ -0,0 +1,37 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+package dk.emstar.jaxb.adapters;
+
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+
+import javax.xml.bind.annotation.adapters.XmlAdapter;
+
+public class UTCLocalDateTimeAdapter extends XmlAdapter<String, LocalDateTime>  {
+
+    public static ZoneId utcTimeZone = ZoneId.of("UTC");
+
+    @Override
+    public String marshal(LocalDateTime localDateTime) throws Exception {
+        if(localDateTime == null) {
+            return null;
+        }
+       
+        return localDateTime.atZone(ZoneId.systemDefault()).withZoneSameInstant(utcTimeZone)
+                .format(DateTimeFormatter.ISO_OFFSET_DATE_TIME);
+    }
+
+    @Override
+    public LocalDateTime unmarshal(String xmlData) throws Exception {
+        if(xmlData == null) {
+            return null;
+        }
+       
+        return ZonedDateTime.parse(xmlData, DateTimeFormatter.ISO_OFFSET_DATE_TIME)
+                .withZoneSameInstant(ZoneId.systemDefault())
+                .toLocalDateTime();
+    }
+}
