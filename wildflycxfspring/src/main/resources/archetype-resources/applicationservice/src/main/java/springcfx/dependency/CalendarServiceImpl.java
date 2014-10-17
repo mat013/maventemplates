7a3c7a801f8eb1c512db5ec93b5a8c8bfe32fb5d
mat013 <mat013@8c0d3757-8840-4923-84ec-c338a6bcf567>
2014-10-17 19:29:19 +0000

    Adding relevant files
    git-svn-id: svn://dkcphbackup01/work/trunk@506 8c0d3757-8840-4923-84ec-c338a6bcf567

---
+++ wildflycxfspring/src/main/resources/archetype-resources/applicationservice/src/main/java/springcfx/dependency/CalendarServiceImpl.java
@@ -0,0 +1,16 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+package ${package}.${parentArtifactId}.dependency;
+
+import java.time.LocalDateTime;
+
+
+public class CalendarServiceImpl implements CalendarService {
+
+    @Override
+    public LocalDateTime now() {
+        return LocalDateTime.now();
+    }
+
+}
