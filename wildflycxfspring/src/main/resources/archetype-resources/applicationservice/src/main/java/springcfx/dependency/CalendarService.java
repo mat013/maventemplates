08816ff393fe6b7c75cb39983b67a10f35320a51
mat013 <mat013@8c0d3757-8840-4923-84ec-c338a6bcf567>
2014-10-17 19:46:01 +0000

    Adding third attempt
    git-svn-id: svn://dkcphbackup01/work/trunk@508 8c0d3757-8840-4923-84ec-c338a6bcf567

---
+++ wildflycxfspring/src/main/resources/archetype-resources/applicationservice/src/main/java/springcfx/dependency/CalendarService.java
@@ -0,0 +1,10 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+package ${package}.${parentArtifactId}.dependency;
+
+import java.time.LocalDateTime;
+
+public interface CalendarService {
+    LocalDateTime now();
+}
