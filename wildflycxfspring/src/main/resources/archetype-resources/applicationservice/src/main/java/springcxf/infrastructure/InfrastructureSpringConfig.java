bce10c1a04172eda2379cacc20a06c1937b47b6f
mat013 <mat013@8c0d3757-8840-4923-84ec-c338a6bcf567>
2014-10-17 23:38:49 +0000

    Adding externalize properties
    git-svn-id: svn://dkcphbackup01/work/trunk@513 8c0d3757-8840-4923-84ec-c338a6bcf567

---
+++ wildflycxfspring/src/main/resources/archetype-resources/applicationservice/src/main/java/springcxf/infrastructure/InfrastructureSpringConfig.java
@@ -0,0 +1,13 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+package ${package}.${parentArtifactId}.infrastructure;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@ComponentScan(basePackageClasses = {InfrastructureSpringConfig.class})
+@Configuration
+public class InfrastructureSpringConfig {
+
+}
---
+++ wildflycxfspring/src/main/resources/archetype-resources/applicationservice/src/main/java/springcxf/services/GreetingServiceBean.java
@@ -0,0 +1,36 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+package ${package}.${parentArtifactId}.services;
+ 
+import javax.inject.Inject;
+import javax.jws.WebService;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import dk.emstar.common.time.ClockService;
+import dk.emstar.example.greeting.Greet;
+import dk.emstar.example.greeting.GreetResponse;
+import dk.emstar.example.greeting.GreetService;
+
+@Service
+@WebService
+public class GreetingServiceBean implements GreetService {
+ 
+    @Inject
+    private ClockService clockService;
+
+    @Value("${symbol_dollar}{environment}")
+    private String environment;
+
+    @Override
+    public GreetResponse greet(Greet request) {
+        GreetResponse response = new GreetResponse();
+        response.setText("Hello " + request.getNavn() + " time is " + clockService.now() + " and the environment is " + environment);
+        response.setDateTime(clockService.now());
+        response.setDate(clockService.dateNow());
+        
+        return response;
+    }
+}
