4e77db2275ee700ad066eee8ee460b825ebacf99
mat013 <mat013@8c0d3757-8840-4923-84ec-c338a6bcf567>
2014-10-18 23:14:07 +0000

    adding go.bat and fixing dynamic folder issue
    git-svn-id: svn://dkcphbackup01/work/trunk@519 8c0d3757-8840-4923-84ec-c338a6bcf567

---
+++ wildflycxfspring/src/main/resources/archetype-resources/applicationservice/src/main/java/__parentArtifactId__/services/ws/GreetingServiceBean.java
@@ -0,0 +1,69 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+package ${package}.${parentArtifactId}.services.ws;
+ 
+import javax.inject.Inject;
+import javax.jws.WebService;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.stereotype.Service;
+
+import dk.emstar.common.time.ClockService;
+import dk.emstar.example.greeting.Greet;
+import dk.emstar.example.greeting.GreetResponse;
+import dk.emstar.example.greeting.GreetService;
+import dk.emstar.example.greeting.SendMessageUsingAdapter;
+import dk.emstar.example.greeting.SendMessageUsingAdapterResponse;
+import dk.emstar.example.greeting.SendMessageUsingInterface;
+import dk.emstar.example.greeting.SendMessageUsingInterfaceResponse;
+
+@Service
+@WebService
+public class GreetingServiceBean implements GreetService {
+    private static final Logger logger = LoggerFactory.getLogger(GreetingServiceBean.class);
+    
+    @Inject
+    private ClockService clockService;
+
+    @Value("${symbol_dollar}{environment}")
+    private String environment;
+
+    @Inject
+    @Qualifier("interfaceOnSameConnectionFactory")
+    private JmsTemplate interfaceOnSameConnectionFactory;
+
+    @Inject
+    @Qualifier("adapterOnSameConnectionFactory")
+    private JmsTemplate adapterOnSameConnectionFactory;
+
+    @Override
+    public GreetResponse greet(Greet request) {
+        GreetResponse response = new GreetResponse();
+        response.setText("Hello " + request.getNavn() + " time is " + clockService.now() + " and the environment is " + environment);
+        response.setDateTime(clockService.now());
+        response.setDate(clockService.dateNow());
+
+        return response;
+    }
+
+    @Override
+    public SendMessageUsingAdapterResponse sendMessageUsingAdapter(SendMessageUsingAdapter request) {
+        String textMessage = request.getMessage();
+        adapterOnSameConnectionFactory.send(o -> o.createTextMessage(textMessage));
+        SendMessageUsingAdapterResponse result = new SendMessageUsingAdapterResponse();
+        return result;
+    }
+
+    @Override
+    public SendMessageUsingInterfaceResponse sendMessageUsingInterface(SendMessageUsingInterface request) {
+        String textMessage = request.getMessage();
+        interfaceOnSameConnectionFactory.send(o -> o.createTextMessage(textMessage));
+        SendMessageUsingInterfaceResponse result = new SendMessageUsingInterfaceResponse();
+        return result;
+    }
+}
---
+++ wildflycxfspring/src/main/resources/archetype-resources/applicationservice/src/main/java/dk/emstar/common/aspect/CorrelationIdWebserviceAspect.java
@@ -0,0 +1,62 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+package dk.emstar.common.aspect;
+
+import java.util.UUID;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.Signature;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
+
+@Aspect 
+public class CorrelationIdWebserviceAspect {
+    private static final String CORRELATION_ID_KEY = "__correlationId";
+    private static final Logger logger = LoggerFactory.getLogger(CorrelationIdWebserviceAspect.class);
+
+    @Pointcut("execution(public * *(..))")
+    public void publicMethod() {
+    }
+
+    @Pointcut("within(@javax.jws.WebService *)")
+    public void webserviceAnnotatedBeans() {
+    }
+
+    @Around("publicMethod()  && webserviceAnnotatedBeans()")
+    public Object logInputOutput(final ProceedingJoinPoint joinPoint) throws Throwable {
+
+        // TODO: find the standard header for the request and see if we can get
+        // the correlationID
+        // For now it will be null and thereby generate a new correlationid
+        UUID correlationId = null;
+        Signature invokedMethodSignature = joinPoint.getSignature();
+
+        boolean isNewCorrelationId = false;
+        if (correlationId == null) {
+            correlationId = UUID.randomUUID();
+            isNewCorrelationId = true;
+        }
+
+        try {
+            MDC.put(CORRELATION_ID_KEY, correlationId.toString());
+            if (isNewCorrelationId) {
+                Class<?> declaringType = invokedMethodSignature.getDeclaringType();
+                logger.info("Generating new correlation id for for invocation {}.{}", declaringType.getSimpleName(), invokedMethodSignature.getName());
+            }
+
+            return joinPoint.proceed();
+        } finally {
+            MDC.remove(CORRELATION_ID_KEY);
+        }
+    }
+
+    public static String getCurrentCorrelationId() {
+        return MDC.get(CORRELATION_ID_KEY);
+    }
+
+}
