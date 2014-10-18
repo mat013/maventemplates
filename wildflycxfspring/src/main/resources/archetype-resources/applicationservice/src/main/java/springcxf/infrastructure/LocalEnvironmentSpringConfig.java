2ee605af45ff338bbe5bfcf35f3b5a0fcf8fc69f
mat013 <mat013@8c0d3757-8840-4923-84ec-c338a6bcf567>
2014-10-18 19:34:43 +0000

    Adding activemq
    git-svn-id: svn://dkcphbackup01/work/trunk@515 8c0d3757-8840-4923-84ec-c338a6bcf567

---
+++ wildflycxfspring/src/main/resources/archetype-resources/applicationservice/src/main/java/springcxf/infrastructure/LocalEnvironmentSpringConfig.java
@@ -0,0 +1,54 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+package ${package}.${parentArtifactId}.infrastructure;
+
+import java.net.URI;
+
+import javax.jms.ConnectionFactory;
+import javax.jms.Queue;
+
+import org.apache.activemq.ActiveMQConnectionFactory;
+import org.apache.activemq.broker.BrokerFactory;
+import org.apache.activemq.broker.BrokerService;
+import org.apache.activemq.command.ActiveMQQueue;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.jms.connection.CachingConnectionFactory;
+
+import dk.emstar.common.spring.Environments;
+
+@PropertySource({"file:${symbol_dollar}{config_home}/${parentArtifactId}/local/environment.properties"})
+@Configuration
+@Profile(Environments.LOCAL)
+public class LocalEnvironmentSpringConfig {
+    @Bean(name = InfrastructureSpringConfig.LISTENER_CONNECTION_FACTORY)
+    public ConnectionFactory listenerConnectionFactory(
+            @Value("${symbol_dollar}{listner.connection.url}") String url) {
+        ActiveMQConnectionFactory result = new ActiveMQConnectionFactory();
+        result.setBrokerURL(url);
+        return cachedConnectionFactoryFor(result);
+    }
+
+    @Bean(name = InfrastructureSpringConfig.LISTENER_QUEUE)
+    public Queue listenerQueue(
+            @Value("${symbol_dollar}{listner.connection.queuename}") String listnerQueueName) {
+        return new ActiveMQQueue(listnerQueueName);
+    }
+
+    @Bean(initMethod = "start", destroyMethod = "stop")
+    public BrokerService mainBroker(
+            @Value("${symbol_dollar}{mainBroker.url}") String url) throws Exception {
+        return BrokerFactory.createBroker(url);
+    }
+
+    private ConnectionFactory cachedConnectionFactoryFor(ConnectionFactory connectionFactory) {
+        CachingConnectionFactory result = new CachingConnectionFactory();
+        result.setTargetConnectionFactory(connectionFactory);
+        result.setSessionCacheSize(50);
+        return result;
+    }
+}
---
+++ wildflycxfspring/src/main/resources/archetype-resources/applicationservice/src/main/java/springcxf/services/ws/GreetingServiceBean.java
@@ -0,0 +1,56 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+package ${package}.${parentArtifactId}.services.ws;
+ 
+import javax.inject.Inject;
+import javax.jws.WebService;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.stereotype.Service;
+
+import dk.emstar.common.time.ClockService;
+import dk.emstar.example.greeting.Greet;
+import dk.emstar.example.greeting.GreetResponse;
+import dk.emstar.example.greeting.GreetService;
+import dk.emstar.example.greeting.SendMessage;
+import dk.emstar.example.greeting.SendMessageResponse;
+
+@Service
+@WebService
+public class GreetingServiceBean implements GreetService {
+    private static final Logger logger = LoggerFactory.getLogger(GreetingServiceBean.class);
+    
+    @Inject
+    private ClockService clockService;
+
+    @Value("${symbol_dollar}{environment}")
+    private String environment;
+
+    @Inject
+    @Qualifier("onSameConnectionFactory")
+    private JmsTemplate onSameConnectionFactory;
+
+    @Override
+    public GreetResponse greet(Greet request) {
+        GreetResponse response = new GreetResponse();
+        response.setText("Hello " + request.getNavn() + " time is " + clockService.now() + " and the environment is " + environment);
+        response.setDateTime(clockService.now());
+        response.setDate(clockService.dateNow());
+
+        return response;
+    }
+
+    @Override
+    public SendMessageResponse sendMessage(SendMessage request) {
+        String textMessage = request.getIn();
+        onSameConnectionFactory.send(o -> o.createTextMessage(textMessage));
+        SendMessageResponse result = new SendMessageResponse();
+        result.setOut("done!");
+        return result;
+    }
+}
