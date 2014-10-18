05545f2d998b3e65836074ef5d10bc0632764b6f
mat013 <mat013@8c0d3757-8840-4923-84ec-c338a6bcf567>
2014-10-18 21:43:34 +0000

    Adding two types of listeners implementations and adding test for outside of JVM call
    git-svn-id: svn://dkcphbackup01/work/trunk@517 8c0d3757-8840-4923-84ec-c338a6bcf567

---
+++ wildflycxfspring/src/main/resources/archetype-resources/applicationservice/src/main/java/springcxf/infrastructure/InfrastructureSpringConfig.java
@@ -0,0 +1,84 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+package ${package}.${parentArtifactId}.infrastructure;
+
+import javax.jms.ConnectionFactory;
+import javax.jms.Queue;
+import javax.jms.Session;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.jms.listener.DefaultMessageListenerContainer;
+import org.springframework.jms.listener.adapter.MessageListenerAdapter;
+
+import ${package}.${parentArtifactId}.services.msg.TextMessageListener;
+import ${package}.${parentArtifactId}.services.msg.TextMessageUsingAdapterListener;
+
+@ComponentScan(basePackageClasses = {InfrastructureSpringConfig.class})
+@Configuration
+public class InfrastructureSpringConfig {
+    public static final String LISTENER_INTERFACE_QUEUE = "listenerInterfaceQueue";
+    public static final String LISTENER_ADAPTER_QUEUE = "listenerAdapterQueue";
+    public static final String LISTENER_CONNECTION_FACTORY = "listenerConnectionFactory";
+
+    @Bean
+    public MessageListenerAdapter textMessageListenerUsingAdapter(TextMessageUsingAdapterListener textMessageUsingAdapterListener) {
+        MessageListenerAdapter messageListener
+                = new MessageListenerAdapter(textMessageUsingAdapterListener);
+        messageListener.setDefaultListenerMethod("onReceive");
+        return messageListener;
+    }
+
+    
+    @Bean
+    public DefaultMessageListenerContainer textMessageListenerContainerUsingAdapter(
+             @Qualifier("textMessageListenerUsingAdapter") MessageListenerAdapter messageListener,
+             @Qualifier(LISTENER_CONNECTION_FACTORY) ConnectionFactory listenerConnectionFactory,
+             @Qualifier(LISTENER_ADAPTER_QUEUE) Queue listenerQueue) {
+        DefaultMessageListenerContainer messageListenerContainer = new DefaultMessageListenerContainer();
+        messageListenerContainer.setMessageListener(messageListener);
+        messageListenerContainer.setConnectionFactory(listenerConnectionFactory);
+        messageListenerContainer.setDestination(listenerQueue);
+        messageListenerContainer.setSessionAcknowledgeMode(Session.AUTO_ACKNOWLEDGE);
+        messageListenerContainer.setConcurrency("20-25");
+        return messageListenerContainer;
+    }
+    
+    @Bean
+    public JmsTemplate adapterOnSameConnectionFactory( 
+            @Qualifier(LISTENER_CONNECTION_FACTORY) ConnectionFactory listenerConnectionFactory,
+            @Qualifier(LISTENER_ADAPTER_QUEUE ) Queue listenerQueue) {
+        JmsTemplate jmsTemplate = new JmsTemplate(listenerConnectionFactory);
+        jmsTemplate.setDefaultDestination(listenerQueue);
+        jmsTemplate.setReceiveTimeout(500);
+        return jmsTemplate;
+    }
+
+   @Bean
+    public DefaultMessageListenerContainer textMessageListenerContainerUsingInterface(TextMessageListener messageListener,
+            @Qualifier(LISTENER_CONNECTION_FACTORY) ConnectionFactory listenerConnectionFactory, 
+            @Qualifier(LISTENER_INTERFACE_QUEUE) Queue listenerQueue) {
+        DefaultMessageListenerContainer messageListenerContainer = new DefaultMessageListenerContainer();
+        messageListenerContainer.setMessageListener(messageListener);
+        messageListenerContainer.setConnectionFactory(listenerConnectionFactory);
+        messageListenerContainer.setDestination(listenerQueue);
+        messageListenerContainer.setSessionAcknowledgeMode(Session.AUTO_ACKNOWLEDGE);
+        messageListenerContainer.setConcurrency("20-25");
+        return messageListenerContainer;
+    }
+
+    @Bean
+    public JmsTemplate interfaceOnSameConnectionFactory( 
+            @Qualifier(LISTENER_CONNECTION_FACTORY) ConnectionFactory listenerConnectionFactory,
+            @Qualifier(LISTENER_INTERFACE_QUEUE ) Queue listenerQueue) {
+        JmsTemplate jmsTemplate = new JmsTemplate(listenerConnectionFactory);
+        jmsTemplate.setDefaultDestination(listenerQueue);
+        jmsTemplate.setReceiveTimeout(500);
+        return jmsTemplate;
+    }
+    
+}
