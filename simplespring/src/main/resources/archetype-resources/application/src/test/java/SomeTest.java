1c39a05cbb81576b59d41422774eaea82990b50f
mat013 <mat013@8c0d3757-8840-4923-84ec-c338a6bcf567>
2014-10-19 19:25:42 +0000

    Adding simple spring archetype
    git-svn-id: svn://dkcphbackup01/work/trunk@521 8c0d3757-8840-4923-84ec-c338a6bcf567

---
+++ simplespring/src/main/resources/archetype-resources/application/src/test/java/SomeTest.java
@@ -0,0 +1,24 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+package ${package};
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+import static org.mockito.Mockito.*;
+
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = {SpringConfig.class})
+@Ignore
+public class SomeTest {
+    @Test
+    public void testName() throws Exception {
+        assertThat("Test not implemented", false);
+    }
+}
