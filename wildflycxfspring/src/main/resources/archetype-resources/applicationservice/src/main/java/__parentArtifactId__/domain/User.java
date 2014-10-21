a9d9a1ec227f298ba98baa8d0f921028d599aae1
mat013 <mat013@8c0d3757-8840-4923-84ec-c338a6bcf567>
2014-10-21 21:24:59 +0000

    Adding database support, BMT, CMT and CrudRepository
    git-svn-id: svn://dkcphbackup01/work/trunk@529 8c0d3757-8840-4923-84ec-c338a6bcf567

---
+++ wildflycxfspring/src/main/resources/archetype-resources/applicationservice/src/main/java/__parentArtifactId__/domain/User.java
@@ -0,0 +1,58 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+package ${package}.${parentArtifactId}.domain;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "Users")
+@SequenceGenerator(name = "user_id_generator",  sequenceName = "users_seq", allocationSize=5)
+public class User implements Serializable {
+
+    private static final long serialVersionUID = -6956783582634460641L;
+
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "user_id_generator")
+    @Id
+    @Column
+    private int id;
+
+    @Column
+    private String username;
+
+    @Column
+    private String password;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
+
---
+++ wildflycxfspring/src/main/resources/archetype-resources/applicationservice/src/main/java/__parentArtifactId__/services/ServicesSpringConfig.java
@@ -0,0 +1,19 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+package ${package}.${parentArtifactId}.services;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+import ${package}.${parentArtifactId}.services.domain.UserGeneratedRepository;
+
+@ComponentScan(basePackageClasses = {ServicesSpringConfig.class})
+@Configuration
+@EnableJpaRepositories(entityManagerFactoryRef="entityManagerFactory", 
+    transactionManagerRef = "transactionManagerJPA", 
+    basePackageClasses={ UserGeneratedRepository.class })
+public class ServicesSpringConfig {
+
+}
