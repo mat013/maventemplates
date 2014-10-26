5f146834e3bd23b9b1cb176ecd0d038b54b6b557
mat013 <mat013@8c0d3757-8840-4923-84ec-c338a6bcf567>
2014-10-26 11:19:46 +0000

    Removing old
    git-svn-id: svn://dkcphbackup01/work/trunk@534 8c0d3757-8840-4923-84ec-c338a6bcf567

--- wildflycxfspring/src/main/resources/archetype-resources/applicationservice/src/main/java/services/msg/TextMessageUsingAdapterListener.java
+++
@@ -1,8 +0,0 @@
-#set( $symbol_pound = '#' )
-#set( $symbol_dollar = '$' )
-#set( $symbol_escape = '\' )
-package ${package}.services.msg;
-
-public interface TextMessageUsingAdapterListener {
-    void onReceive(String message);
-}
