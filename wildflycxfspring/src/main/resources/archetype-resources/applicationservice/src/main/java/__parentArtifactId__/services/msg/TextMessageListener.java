4dfd1ab455c329550d561705f646a1b8ec2dddc1
mat013 <mat013@8c0d3757-8840-4923-84ec-c338a6bcf567>
2014-10-26 09:50:10 +0000

    Removing old
    git-svn-id: svn://dkcphbackup01/work/trunk@531 8c0d3757-8840-4923-84ec-c338a6bcf567

--- wildflycxfspring/src/main/resources/archetype-resources/applicationservice/src/main/java/__parentArtifactId__/services/msg/TextMessageListener.java
+++
@@ -1,7 +0,0 @@
-#set( $symbol_pound = '#' )
-#set( $symbol_dollar = '$' )
-#set( $symbol_escape = '\' )
-package ${package}.${parentArtifactId}.services.msg;
-
-public interface TextMessageListener {
-}
--- wildflycxfspring/src/main/resources/archetype-resources/interfaces/src/itest/java/__parentArtifactId__/GreetingServiceBeanITest.java
+++
@@ -1,9 +0,0 @@
-#set( $symbol_pound = '#' )
-#set( $symbol_dollar = '$' )
-#set( $symbol_escape = '\' )
-package ${package}.${parentArtifactId};
- 
-
-public class GreetingServiceBeanITest {
-    // TODO: to be removed 
-}
