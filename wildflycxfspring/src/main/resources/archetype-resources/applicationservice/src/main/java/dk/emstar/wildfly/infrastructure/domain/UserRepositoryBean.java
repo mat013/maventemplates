8d9ee0daf3d4c788e372b6be8df7dca11b3853fc
mat013 <mat013@8c0d3757-8840-4923-84ec-c338a6bcf567>
2014-10-26 11:20:20 +0000

    Adding querydsl
    git-svn-id: svn://dkcphbackup01/work/trunk@535 8c0d3757-8840-4923-84ec-c338a6bcf567

---
+++ wildflycxfspring/src/main/resources/archetype-resources/applicationservice/src/main/java/dk/emstar/wildfly/infrastructure/domain/UserRepositoryBean.java
@@ -0,0 +1,46 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+package dk.emstar.wildfly.infrastructure.domain;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.TransactionDefinition;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
+
+import dk.emstar.wildfly.domain.User;
+import dk.emstar.wildfly.services.domain.UserRepository;
+
+@Repository("userRepository")
+public class UserRepositoryBean implements UserRepository {
+
+    @Autowired
+    private JpaTransactionManager txManager;
+    
+    @PersistenceContext(name="test")
+    private EntityManager entityManager;
+
+    @Transactional
+    @Override
+    public void createUser(User user) {
+        entityManager.persist(user);
+    }
+
+    @Override
+    public void createUserBMT(User user) {
+        DefaultTransactionDefinition def = new DefaultTransactionDefinition();
+        def.setName("rootTransaction");
+        def.setPropagationBehavior(TransactionDefinition.PROPAGATION_REQUIRED);
+        TransactionStatus status = txManager.getTransaction(def);
+        entityManager.persist(user);
+        txManager.commit(status);
+    }
+
+}
+
---
+++ wildflycxfspring/src/main/resources/archetype-resources/applicationservice/src/main/java/dk/emstar/wildfly/services/msg/TextMessageListener.java
@@ -0,0 +1,7 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+package dk.emstar.wildfly.services.msg;
+
+public interface TextMessageListener {
+}
