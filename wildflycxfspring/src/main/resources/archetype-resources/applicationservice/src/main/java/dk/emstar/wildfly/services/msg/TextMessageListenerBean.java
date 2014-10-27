ea811086d077f9bce4922a79da1d3183ad6f8584
mat013 <mat013@8c0d3757-8840-4923-84ec-c338a6bcf567>
2014-10-27 20:05:05 +0000

    Adding custom interceptor
    git-svn-id: svn://dkcphbackup01/work/trunk@540 8c0d3757-8840-4923-84ec-c338a6bcf567

---
+++ wildflycxfspring/src/main/resources/archetype-resources/applicationservice/src/main/java/dk/emstar/wildfly/services/msg/TextMessageListenerBean.java
@@ -0,0 +1,31 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+package dk.emstar.wildfly.services.msg;
+
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageListener;
+import javax.jms.TextMessage;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TextMessageListenerBean implements MessageListener, TextMessageListener {
+
+    private static final Logger logger = LoggerFactory.getLogger(MessageListener.class);
+
+    public void onMessage(Message message) {
+        try {
+            if (message instanceof TextMessage) {
+                TextMessage tm = (TextMessage) message;
+                String msg = tm.getText();
+                logger.info("onMessage processed message '{}'", msg);
+            }
+        } catch (JMSException e) {
+            logger.error(e.getMessage(), e);
+        }
+    }
+}
